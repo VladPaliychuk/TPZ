a766fbe42156e77e86dd6280b80c68b597ef0bc7
VladPaliychuk <ssmxtxl@gmail.com>
2024-09-11 14:07:58 +0300

    create ConverterRomanToInt.cs and ConverterRomanToIntTest.cs

---
+++ NUnitTests/Tests/ConverterRomanToIntTest.cs
@@ -0,0 +1,114 @@
+﻿using NUnit.Framework;
+using TPZ.Repositories;
+
+namespace NUnitTests.Tests
+{
+    [TestFixture]
+    public class ConverterRomanToIntTest
+    {
+        private ConverterRomanToInt _converter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _converter = new ConverterRomanToInt();
+        }
+
+        // 10 Tests for exceptions
+        [Test]
+        public void whenRomanNumeralIsInvalid_ThenThrowException_1()
+        {
+            Assert.Throws<System.ArgumentException>(() => _converter.Convert("IIII"));
+        }
+
+        [Test]
+        public void whenRomanNumeralIsInvalid_ThenThrowException_2()
+        {
+            Assert.Throws<System.ArgumentException>(() => _converter.Convert("VV"));
+        }
+
+        [Test]
+        public void whenRomanNumeralIsCyrillic_ThenThrowException()
+        {
+            Assert.Throws<System.ArgumentException>(() => _converter.Convert("Рим"));
+        }
+
+        [Test]
+        public void whenRomanNumeralIsInteger_ThenThrowException()
+        {
+            Assert.Throws<System.ArgumentException>(() => _converter.Convert("123"));
+        }
+
+        [Test]
+        public void whenRomanNumeralIsEmpty_ThenThrowException()
+        {
+            Assert.Throws<System.ArgumentException>(() => _converter.Convert(""));
+        }
+        
+        [Test]
+        public void whenRomanNumeralIsLowerCase_ThenThrowException()
+        {
+            Assert.Throws<System.ArgumentException>(() => _converter.Convert("iv"));
+        }
+        
+        [Test]
+        public void whenRomanNumeralIsMixedCase_ThenThrowException()
+        {
+            Assert.Throws<System.ArgumentException>(() => _converter.Convert("iV"));
+        }
+        
+        [Test]
+        public void whenRomanNumeralIsWhitespace_ThenThrowException()
+        {
+            Assert.Throws<System.ArgumentException>(() => _converter.Convert(" "));
+        }
+        
+        [Test]
+        public void whenRomanNumeralIsNull_ThenThrowException()
+        {
+            Assert.Throws<System.ArgumentNullException>(() => _converter.Convert(null));
+        }
+
+        [Test]
+        public void whenRomanNumeralIsSpecialCharacters_ThenThrowException()
+        {
+            Assert.Throws<System.ArgumentException>(() => _converter.Convert("@#$%"));
+        }
+
+        // 5 Tests for conversion
+        [Test]
+        public void whenRomanNumeralIs_I_ThenReturn_1()
+        {
+            var result = _converter.Convert("I");
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void whenRomanNumeralIs_IV_ThenReturn_4()
+        {
+            var result = _converter.Convert("IV");
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void whenRomanNumeralIs_XL_ThenReturn_40()
+        {
+            var result = _converter.Convert("XL");
+            Assert.AreEqual(40, result);
+        }
+
+        [Test]
+        public void whenRomanNumeralIs_CM_ThenReturn_900()
+        {
+            var result = _converter.Convert("CM");
+            Assert.AreEqual(900, result);
+        }
+
+        [Test]
+        public void whenRomanNumeralIs_MCMXCIV_ThenReturn_1994()
+        {
+            var result = _converter.Convert("MCMXCIV");
+            Assert.AreEqual(1994, result);
+        }
+    }
+}
---
+++ Repositories/ConverterRomanToInt.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace TPZ.Repositories
+{
+    public class ConverterRomanToInt
+    {
+        public ConverterRomanToInt()
+        {
+            // Empty constructor
+        }
+        
+        public int Convert(string roman)
+        {
+            if (!IsValid(roman))
+            {
+                throw new System.ArgumentException("Invalid Roman numeral");
+            }
+            
+            int result = 0;
+            int i = 0;
+            
+            while (i < roman.Length)
+            {
+                int s1 = Value(roman[i]);
+                
+                if (i + 1 < roman.Length)
+                {
+                    int s2 = Value(roman[i + 1]);
+                    
+                    if (s1 >= s2)
+                    {
+                        result = result + s1;
+                        i++;
+                    }
+                    else
+                    {
+                        result = result + s2 - s1;
+                        i = i + 2;
+                    }
+                }
+                else
+                {
+                    result = result + s1;
+                    i++;
+                }
+            }
+            
+            return result;
+        }
+        
+        private int Value(char r)
+        {
+            switch (r)
+            {
+                case 'I':
+                    return 1;
+                case 'V':
+                    return 5;
+                case 'X':
+                    return 10;
+                case 'L':
+                    return 50;
+                case 'C':
+                    return 100;
+                case 'D':
+                    return 500;
+                case 'M':
+                    return 1000;
+                default:
+                    return -1;
+            }
+        }
+        
+        public bool IsValid(string roman)
+        {
+            if (roman == null)
+            {
+                throw new ArgumentNullException(nameof(roman), "Roman numeral cannot be null");
+            }
+
+            if (roman == null || roman.Trim().Length == 0)
+            {
+                throw new ArgumentException("Roman numeral cannot be null, empty, or whitespace", nameof(roman));
+            }
+
+            foreach (var c in roman)
+            {
+                if (c != 'I' && c != 'V' && c != 'X' && c != 'L' && c != 'C' && c != 'D' && c != 'M')
+                {
+                    throw new ArgumentException($"Invalid character '{c}' in Roman numeral", nameof(roman));
+                }
+            }
+            
+            if (roman.Contains("IIII") || roman.Contains("VV") || roman.Contains("XXXX") || 
+                roman.Contains("LL") || roman.Contains("CCCC") || roman.Contains("DD") || 
+                roman.Contains("MMMM"))
+            {
+                throw new ArgumentException("Invalid sequence in Roman numeral", nameof(roman));
+            }
+
+            return true;
+        }
+    }
+}
